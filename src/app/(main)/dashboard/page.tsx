244c3f8d7646e946261dfe963548db26bce935f4
KrishnaKant Nagvanshi <krishnakantbha@gmail.com>
2025-10-23 01:17:15 +0530

    Getting token from the URL after OAuth2 login

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,6 +1,22 @@
-import React from "react";
+"use client";
+import { setJwtToken } from "@/store/features/slices/authSlice";
+import { useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const Dashboard = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = searchParams.get("token");
+    if (token) {
+      dispatch(setJwtToken(token));
+      router.replace("/dashboard");
+    }
+  }, [searchParams, router, dispatch]);
+  
   return (
     <div>
       <h1>Dashboard</h1>
